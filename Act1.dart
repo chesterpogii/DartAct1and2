3ff11a4e6ca35435a2d4c49fe945293ae96d5868
Don Chester Jimenez <donchesterjimenez4@gmail.com>
2025-08-07 09:45:33 +0000

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,26 @@
+Input:
+
+void main () {
+  String greeting = "Welcome";
+  String user = "Alice";
+  
+  print ("$greeting, $user!");
+  dynamic userDynamic = 25;
+  
+  print ("$greeting, $userDynamic");
+  
+  
+}
+
+
+Output:
+
+Welcome, Alice!
+Welcome, 25
+
+
+Explanation:
+
+manipulating dynamics offers enhanced flexibility in my code because 
+the type of the variable can change during the execution without causing 
+compilation problems.
